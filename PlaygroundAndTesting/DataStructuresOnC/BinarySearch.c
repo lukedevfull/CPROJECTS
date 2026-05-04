700741f265065e3d8c24905747f0c98c06b40221
Luke <contato.prof2@gmail.com>
2026-05-04 20:26:51 -0300

    Fix binary search syntax error and add gitignore
    - Fixed else if/else placement in buscaBinaria function
    - Added return -1 for not found case
    - Added .gitignore to exclude executables and VS Code config

---
+++ PlaygroundAndTesting/DataStructuresOnC/BinarySearch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int buscaBinaria(int arr[], int size, int target) {
+    int init = 0;
+    int end = size - 1;
+    
+    while (init <= end) {
+        int mid = init + (end - init) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target){
+            init = mid + 1;  
+        }
+        else {
+            end = mid -1;
+        }
+        
+    }
+    return -1;
+}
+int main() {
+    int inteiros[] = {1,2,3,4,4,5,5,6,7,8,9,10};
+    int size = sizeof(inteiros) / sizeof(inteiros[0]);
+    int target = 7;
+
+    int result = buscaBinaria(inteiros,size, target);
+    printf("ENCONTRADO EM: %d\n", result);
+    return 0;
+}
