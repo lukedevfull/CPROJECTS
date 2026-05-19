542cd04333130d7617d9aa68b95423d28e4d736e
Luke <contato.prof2@gmailcom>
2026-05-19 18:36:38 -0300

    feat: Binary search implementation with linear  search comparison

--- PlaygroundAndTesting/DataStructuresOnC/BinarySearch.c
+++ PlaygroundAndTesting/DataStructuresOnC/BinarySearch.c
@@ -20,13 +20,27 @@ int buscaBinaria(int arr[], int size, int target) {
     }
     return -1;
 }
+
+int linearSearch(int arr[], int target, int size){
+    for (int i =0; i < size; i++){
+        if (arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
 int main() {
     int inteiros[] = {1,2,3,4,4,5,5,6,7,8,9,10};
     int size = sizeof(inteiros) / sizeof(inteiros[0]);
-    int target = 7;
+    int target = 4;
 
     int result = buscaBinaria(inteiros,size, target);
     printf("ENCONTRADO EM: %d\n", result);
+
+    printf("==========================\n");
+    
+    int result2 = linearSearch(inteiros, target, size);
+    printf("ENCONTRADO EM: %d\n", result2);
     return 0;
 }
  
